acb893522ad303cedc002d82f844a57e5d5d5a28
Gabriel Pinto Andrade <pintoandradegabriel@gmail.com>
2025-04-15 11:42:59 -0300

    update: new api transactions query

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { useGetTransactionsQuery } from "@/lib/features/api/api";
+
 const Transactions = () => {
-  return <div>Transactions</div>;
+  const { data, isLoading } = useGetTransactionsQuery();
+
+  const transactions = data;
+
+  return (
+    <div>
+      <h1>Transactions</h1>
+      {isLoading ? (
+        <p>Carregando ...</p>
+      ) : (
+        transactions?.map((item) => (
+          <div key={item.id}>
+            <p>{item.amount}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Transactions;
